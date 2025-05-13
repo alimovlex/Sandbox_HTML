8264e8bdbd9535831e31227de7608ee232077239
robot <robot@recompile.me>
2025-05-13 11:06:08 +0200

    Implemented the curl function for making a get request for obtaining weather data.

--- backend/src/webpage.c
+++ backend/src/webpage.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include "weather_info.h"
 
 int generate_website() {
+    char *str = "Hello World!";
+    char *title = "Piscine Web";
+    get_weather_info();
     printf("Content-type:text/html\n\n");
-	printf("<html><head><title>Piscine Web</title></head><body>");
-	printf("<h1>Hello World!</h1>");
+	printf("<html><head><title>%s", title);
+    printf("</title></head><body>");
+	printf("<h1><center>%s", str);
+    printf("</center></h1>");
 	printf("</body></html>");
 	printf("\n");
     return 0;
