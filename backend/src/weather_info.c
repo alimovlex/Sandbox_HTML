6f62e8205fa504c48127269dbc4cc644b2abc405
robot <robot@recompile.me>
2025-05-14 11:51:18 +0200

    Solved segfault issue with string concatenation.

---
+++ backend/src/weather_info.c
@@ -0,0 +1,203 @@
+/*
+    weather_info.c
+    webc.cgi
+
+    Created by alimovlex.
+    Copyright (c) 2025 alimovlex. All rights reserved.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <json-c/json.h>
+#include <curl/curl.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+/* holder for curl fetch */
+struct curl_fetch_st {
+    char *payload;
+    size_t size;
+};
+
+/* callback for curl fetch */
+size_t curl_callback (void *contents, size_t size, size_t nmemb, void *userp) {
+    size_t realsize = size * nmemb;                             /* calculate buffer size */
+    struct curl_fetch_st *p = (struct curl_fetch_st *) userp;   /* cast pointer to fetch struct */
+
+    /* expand buffer using a temporary pointer to avoid memory leaks */
+    char * temp = realloc(p->payload, p->size + realsize + 1);
+
+    /* check allocation */
+    if (temp == NULL) {
+        /* this isn't good */
+        fprintf(stderr, "ERROR: Failed to expand buffer in curl_callback");
+        /* free buffer */
+        free(p->payload);
+        /* return */
+        return 1;
+    }
+
+    /* assign payload */
+    p->payload = temp;
+
+    /* copy contents to buffer */
+    memcpy(&(p->payload[p->size]), contents, realsize);
+
+    /* set new buffer size */
+    p->size += realsize;
+
+    /* ensure null termination */
+    p->payload[p->size] = 0;
+
+    /* return size */
+    return realsize;
+}
+
+/* fetch and return url body via curl */
+CURLcode curl_fetch_url(CURL *ch, const char *url, struct curl_fetch_st *fetch) {
+    CURLcode rcode;                   /* curl result code */
+
+    /* init payload */
+    fetch->payload = (char *) calloc(1, sizeof(fetch->payload));
+
+    /* check payload */
+    if (fetch->payload == NULL) {
+        /* log error */
+        fprintf(stderr, "ERROR: Failed to allocate payload in curl_fetch_url");
+        /* return error */
+        return CURLE_FAILED_INIT;
+    }
+
+    /* init size */
+    fetch->size = 0;
+
+    /* set url to fetch */
+    curl_easy_setopt(ch, CURLOPT_URL, url);
+
+    /* set calback function */
+    curl_easy_setopt(ch, CURLOPT_WRITEFUNCTION, curl_callback);
+
+    /* pass fetch struct pointer */
+    curl_easy_setopt(ch, CURLOPT_WRITEDATA, (void *) fetch);
+
+    /* set default user agent */
+    curl_easy_setopt(ch, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+
+    /* set timeout */
+    curl_easy_setopt(ch, CURLOPT_TIMEOUT, 15);
+
+    /* enable location redirects */
+    curl_easy_setopt(ch, CURLOPT_FOLLOWLOCATION, 1);
+
+    /* set maximum allowed redirects */
+    curl_easy_setopt(ch, CURLOPT_MAXREDIRS, 1);
+
+    /* fetch the url */
+    rcode = curl_easy_perform(ch);
+
+    /* return */
+    return rcode;
+}
+
+void parse_json(json_object *json, char* key) {
+    struct json_object *json_value;
+    const char *value;
+    json_value = json_object_object_get(json, key);
+    value = json_object_get_string(json_value);
+
+    if (strcmp(key, "query") == 0)
+    {
+        printf("Public IP: %s\n", value);
+
+        // do something
+    } else if (strlen(value) == 0) {
+        return;
+    } else {
+        printf("%s: %s\n",key, value);
+    }
+
+}
+
+int get_weather_info() {
+    int i = 0;
+    char *CITY = "";
+    char *BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
+    char *KEY = "";
+    char url[1024];
+    snprintf(url, sizeof(url), "%s%s%s%s%s", BASE_URL, CITY, "&appid=", KEY, "&units=metric");
+    printf("%s\n", url);
+
+    CURL *ch;                                               /* curl handle */
+    CURLcode rcode;                                         /* curl result code */
+
+    json_object *json;                                      /* json post body */
+    enum json_tokener_error jerr = json_tokener_success;    /* json parse error */
+
+    struct curl_fetch_st curl_fetch;                        /* curl fetch struct */
+    struct curl_fetch_st *cf = &curl_fetch;                 /* pointer to fetch struct */
+    struct curl_slist *headers = NULL;                      /* http headers to send with request */
+
+    /* init curl handle */
+    if ((ch = curl_easy_init()) == NULL) {
+        /* log error */
+        fprintf(stderr, "ERROR: Failed to create curl handle in fetch_session");
+        /* return error */
+        return 1;
+    }
+
+    /* fetch page and capture return code */
+    rcode = curl_fetch_url(ch, url, cf);
+
+    /* check return code */
+    if (rcode != CURLE_OK || cf->size < 1) {
+        /* log error */
+        fprintf(stderr, "ERROR: Failed to fetch url (%s) - curl said: %s",
+                url, curl_easy_strerror(rcode));
+        /* return error */
+        return 1;
+    }
+
+    /* check payload */
+    if (cf->payload != NULL) {
+        /* print result */
+        printf("CURL Returned: \n%s\n", cf->payload);
+        /* parse return */
+        json = json_tokener_parse_verbose(cf->payload, &jerr);
+
+        /* free payload */
+        free(cf->payload);
+    } else {
+        /* error */
+        fprintf(stderr, "ERROR: Failed to populate payload");
+        /* free payload */
+        free(cf->payload);
+        /* return */
+        return 1;
+    }
+    parse_json(json ,"coord");
+
+    return 0;
+}
+
+/*
+    headers = curl_slist_append(headers, "Accept: application/json");
+    headers = curl_slist_append(headers, "Content-Type: application/json");
+
+    build post data
+    json_object_object_add(json, "title", json_object_new_string("testies"));
+    json_object_object_add(json, "body", json_object_new_string("testies ... testies ... 1,2,3"));
+    json_object_object_add(json, "userId", json_object_new_int(133));
+
+    set curl options
+    curl_easy_setopt(ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_easy_setopt(ch, CURLOPT_HTTPHEADER, headers);
+    curl_easy_setopt(ch, CURLOPT_POSTFIELDS, json_object_to_json_string(json));
+*/
+
